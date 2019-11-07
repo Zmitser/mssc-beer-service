9f1f259023ec6f5ae943d7ef8a19dd2e9a3e2495
Dmitriy Borisovets <gagat@tut.by>
2019-11-07 11:25:29 +0300

    adding validation

--- src/main/kotlin/by/zmitser/msscbeerservice/web/model/BeerDto.kt
+++ src/main/kotlin/by/zmitser/msscbeerservice/web/model/BeerDto.kt
@@ -3,16 +3,32 @@ package by.zmitser.msscbeerservice.web.model
 import java.math.BigDecimal
 import java.time.OffsetDateTime
 import java.util.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Null
+import javax.validation.constraints.Positive
 
-data class BeerDto(val id: UUID?,
-                   val version: Int?,
-                   val creationDate: OffsetDateTime?,
-                   val lastModifiedDate: OffsetDateTime?,
-                   val beerStyle: BeerStyleEnum?,
-                   val upc: Long?,
-                   val price: BigDecimal?,
-                   val quantityOnHand: Int?) {
-    constructor() : this(null, null, null, null, null, null, null, null)
+data class BeerDto(
+        @Null
+        val id: UUID?,
+        @Null
+        val version: Int?,
+        @Null
+        val creationDate: OffsetDateTime?,
+        @Null
+        val lastModifiedDate: OffsetDateTime?,
+        @NotBlank
+        val beerName: String?,
+        @NotNull
+        val beerStyle: BeerStyleEnum?,
+        @NotNull
+        @Positive
+        val upc: Long?,
+        @NotNull
+        @Positive
+        val price: BigDecimal?,
+        val quantityOnHand: Int?) {
+    constructor() : this(null, null, null, null, null, null, null, null, null)
 
     enum class BeerStyleEnum {
         LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAILSON
